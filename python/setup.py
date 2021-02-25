73d0464f109300808fa380069af06dc2497f3458
vnatesh <vikas.natesh@gmail.com>
2021-02-25 07:08:35 -0500

    small fix

--- python/setup.py
+++ python/setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
+from pathlib import Path
+import os
+
+cwd = os.getcwd()
+path = Path(cwd)
+path = str(path.parent) + '/src'
+
 
 setup(name='linear_cpp',
       ext_modules=[cpp_extension.CppExtension(name = 'linear_cpp', 
-                sources = ['../src/linear.cpp', 
-                			'../src/block_sizing.cpp', 
-                            '../src/cake_dgemm.cpp', 
-                            '../src/pack.cpp', 
-                            '../src/util.cpp', 
-                            '../src/unpack.cpp'],
+                sources = ['%s/linear.cpp' % path, 
+                			'%s/block_sizing.cpp' % path, 
+                            '%s/cake_dgemm.cpp' % path, 
+                            '%s/pack.cpp' % path, 
+                            '%s/util.cpp' % path, 
+                            '%s/unpack.cpp' % path],
                 extra_compile_args = ['-O3','-Wall','-Wno-unused-function','-Wfatal-errors', 
                 '-fopenmp', '-fPIC','-D_POSIX_C_SOURCE=200112L', '-DBLIS_VERSION_STRING=\"0.8.0-13\"'],
                 extra_link_args = ['/usr/local/lib/libblis.a', '-lm', '-lpthread', '-lrt'],
--- vtune_prof.sh
+++ vtune_prof.sh
@@ -3,7 +3,7 @@
 
 # run matmul bench for 1..10 threads through intel vtune
 # source /opt/intel/oneapi/setvars.sh;
-cd /home/vnatesh/Documents/;
+cd /home/vnatesh/Documents/cake_pytorch/;
 mkdir reports;
 
 
@@ -20,8 +20,8 @@ make;
 for i in {2..10}
 do
 	vtune --collect memory-access -data-limit=0 \
-		-result-dir=/home/vnatesh/Documents/prof_result \
-	 /home/vnatesh/Documents/mkl_dgemm_test $i; 
+		-result-dir=/home/vnatesh/Documents/cake_pytorch/prof_result \
+	 /home/vnatesh/Documents/cake_pytorch/mkl_dgemm_test $i; 
 
 	vtune -report summary -r prof_result -format csv \
 		-report-output reports/report_mkl_$i.csv -csv-delimiter comma;
@@ -30,12 +30,13 @@ do
 done
 
 
-array=( 2 3 4 5 6 8 10 )
-for i in "${array[@]}"
+# array=( 2 3 4 5 6 8 10 )
+# for i in "${array[@]}"
+for i in {2..10}
 do
 	vtune --collect memory-access -data-limit=0 \
-		-result-dir=/home/vnatesh/Documents/cake_dgemm_result \
-		 /home/vnatesh/Documents/cake_dgemm_test.x $i;
+		-result-dir=/home/vnatesh/Documents/cake_pytorch/cake_dgemm_result \
+		 /home/vnatesh/Documents/cake_pytorch/cake_dgemm_test.x $i;
 
 	vtune -report summary -r cake_dgemm_result -format csv \
 		-report-output reports/report_cake_dgemm_$i.csv -csv-delimiter comma;
@@ -44,5 +45,5 @@ do
 done
 
 # python python/plots.py
-# scp -r plots vikas@10.0.0.185:/Users/vikas/Documents/test/;
-scp -r reports vikas@10.0.0.185:/Users/vikas/Documents/test/;
+# scp -r plots vikas@10.0.0.185:/Users/vikas/Documents/cake_pytorch/test/;
+scp -r reports vikas@10.0.0.185:/Users/vikas/Documents/cake_pytorch/test/;
