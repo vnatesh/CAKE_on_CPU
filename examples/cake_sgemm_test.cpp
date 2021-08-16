7fa233b547ed3b6000c8493931c3d362b74180c4
vnatesh <vikas.natesh@gmail.com>
2021-08-16 15:51:41 -0400

    small fix

--- examples/cake_sgemm_test.cpp
+++ examples/cake_sgemm_test.cpp
@@ -13,7 +13,7 @@ int main( int argc, char** argv ) {
 	M = atoi(argv[1]);
 	K = atoi(argv[2]);
 	N = atoi(argv[3]);
-	p = 10;
+	p = atoi(argv[4]);
 
 	printf("M = %d, K = %d, N = %d, cores = %d\n", M,K,N,p);
 
@@ -27,8 +27,17 @@ int main( int argc, char** argv ) {
 	rand_init(B, K, N);
 
 	cake_cntx_t* cake_cntx = cake_query_cntx();
+	clock_gettime(CLOCK_REALTIME, &start);
+
 	cake_sgemm(A, B, C, M, N, K, p, cake_cntx);
-	cake_sgemm_checker(A, B, C, N, M, K);
+
+    clock_gettime(CLOCK_REALTIME, &end);
+    long seconds = end.tv_sec - start.tv_sec;
+    long nanoseconds = end.tv_nsec - start.tv_nsec;
+    diff_t = seconds + nanoseconds*1e-9;
+	printf("sgemm time: %f \n", diff_t); 
+
+	// cake_sgemm_checker(A, B, C, N, M, K);
 	
 	free(A);
 	free(B);
