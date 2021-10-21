83d829c3d816462dc6af5fc755467c565d996bec
vnatesh <vikas.natesh@gmail.com>
2021-10-21 17:53:50 -0400

    refactor initialization of block dims

--- include/cake.h
+++ include/cake.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h> 
@@ -10,6 +12,29 @@
 #define DEBUG 0
 #define ARR_PRINT 0
 #define CHECK_PRINT 0
+
+
+extern int m_c;
+extern int k_c;
+extern int n_c;
+extern int m_c1;
+extern int k_c1;
+extern int n_c1;
+extern int m_c1_last_core;
+extern int k_c1_last_core;
+extern int mr_rem;
+extern int nr_rem;
+extern int k_rem;
+extern int p_l;
+extern int m_pad;
+extern int k_pad;
+extern int n_pad;
+extern int Mb;
+extern int Kb;
+extern int Nb;
+extern int M_padded;
+extern int N_padded;
+
 
 enum sched {KMN, MKN, NKM};
 
@@ -55,17 +80,17 @@ cake_cntx_t* cake_query_cntx_torch(int L2, int L3);
 Pack 
 
 */
-void pack_B(float* B, float* B_p, int K, int N, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
+void pack_B(float* B, float* B_p, int K, int N, cake_cntx_t* cake_cntx);
 
-double pack_A_single_buf(float* A, float* A_p, int M, int K, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
-void pack_C_single_buf(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
+double pack_A_single_buf(float* A, float* A_p, int M, int K, int p, cake_cntx_t* cake_cntx);
+void pack_C_single_buf(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx);
 
-double pack_A_multiple_buf(float* A, float** A_p, int M, int K, int m_c, int k_c, int m_r, int p);
-void pack_C_multiple_buf(float* C, float** C_p, int M, int N, int m_c, int n_c, int m_r, int n_r, int p, int alpha_n);
+// double pack_A_multiple_buf(float* A, float** A_p, int M, int K, int m_c, int k_c, int m_r, int p);
+// void pack_C_multiple_buf(float* C, float** C_p, int M, int N, int m_c, int n_c, int m_r, int n_r, int p, int alpha_n);
 
 
-void unpack_C_single_buf(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
-void unpack_C_single_buf_m_first(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
+void unpack_C_single_buf(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx);
+void unpack_C_single_buf_m_first(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx);
 void unpack_C_rsc(float* C, float** C_p, int M, int N, int m_c, int n_c, int n_r, int m_r, int p, int alpha_n);
 void unpack_C_old(float* C, float** C_p, int M, int N, int m_c, int n_c, int n_r, int m_r, int p);
 
@@ -89,13 +114,13 @@ void unpack_ob_C_single_buf(float* C, float* C_p, int M, int N, int m1, int n1, 
 void unpack_ob_C_multiple_buf(float* C, float* C_p, int M, int N, int m1, int n1, int m2,
 				int m_c, int n_c, int m_r, int n_r);
 
-double pack_A_single_buf_m_first(float* A, float* A_p, int M, int K, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
-void pack_B_m_first(float* B, float* B_p, int K, int N, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
-void pack_C_single_buf_m_first(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
+double pack_A_single_buf_m_first(float* A, float* A_p, int M, int K, int p, cake_cntx_t* cake_cntx);
+void pack_B_m_first(float* B, float* B_p, int K, int N, int p, cake_cntx_t* cake_cntx);
+void pack_C_single_buf_m_first(float* C, float* C_p, int M, int N, int p, cake_cntx_t* cake_cntx);
 
-int cake_sgemm_packed_A_size(int M, int K, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
-int cake_sgemm_packed_B_size(int K, int N, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
-int cake_sgemm_packed_C_size(int M, int N, int p, cake_cntx_t* cake_cntx, blk_dims_t* blk_dims);
+int cake_sgemm_packed_A_size(int M, int K, int p, cake_cntx_t* cake_cntx);
+int cake_sgemm_packed_B_size(int K, int N, int p, cake_cntx_t* cake_cntx);
+int cake_sgemm_packed_C_size(int M, int N, int p, cake_cntx_t* cake_cntx);
 
 double cake_sgemm(float* A, float* B, float* C, int M, int N, int K, int p, 
 	cake_cntx_t* cake_cntx, bool packedA = 0, bool packedB = 0, float alpha = 1, float beta = 0);
@@ -127,4 +152,7 @@ bool add_checker(float** C_arr, float* C, int M, int N, int p);
 void rand_init(float* mat, int r, int c);
 
 void print_array(float* arr, int len);
+
+
+void init_block_dims(int M, int N, int K, int p, cake_cntx_t* cake_cntx, enum sched sch) ;
 
