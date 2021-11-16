9ecb815e3188ba5b0310095a25707f55ee175592
vnatesh <vikas.natesh@gmail.com>
2021-11-16 03:46:33 -0500

    move thread launcher to cake_on_net

--- src/cake_sgemm.cpp
+++ src/cake_sgemm.cpp
@@ -17,14 +17,3 @@ double cake_sgemm(float* A, float* B, float* C, int M, int N, int K, int p,
 }
 
 
-// launching CAKE sgemm on asynchronous thread
-
-void* cake_sgemm_launch(void* inputs) {
-	
-    struct gemm_input* inp = (struct gemm_input*) inputs;    
-    double ans = cake_sgemm(inp->A, inp->B, inp->C, inp->M, inp->N, inp->K, inp->p, 
-    	inp->cake_cntx, inp->packedA, inp->packedB, inp->alpha, inp->beta);
-	//sleep(4);
-	pthread_exit(NULL);
-}
-
