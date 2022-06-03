6d5c309a4929dc9e3f59271fedf33ae6dddadcc5
vnatesh <vikas.natesh@gmail.com>
2022-06-03 13:20:05 -0400

    new exp

---
+++ experiments/amd/skew/cake_sgemm_test.cpp
@@ -0,0 +1,120 @@
+#include "cake.h"
+
+
+
+
+int main( int argc, char** argv ) {
+//	  run_tests();
+
+	int M, K, N, p = 10, ntrials = 100;
+	struct timespec start, end;
+	double diff_t, ans;
+	float *A, *B, *C;
+	long seconds, nanoseconds;
+    cake_cntx_t* cake_cntx = cake_query_cntx();
+
+	char fname[50];
+	snprintf(fname, sizeof(fname), "result_skew");
+
+    FILE *fp;
+    fp = fopen(fname, "a");
+    fprintf(fp, "algo,M,K,N,time\n");
+    fclose(fp);
+
+
+	K = atoi(argv[1]);
+	N = atoi(argv[2]);
+
+	for(int i = 0; i < ntrials; i++) {
+		for(M = 500; M < 20001; M += 500) {
+
+			A = (float*) malloc(M * K * sizeof( float ));
+			B = (float*) malloc(K * N * sizeof( float ));
+			C = (float*) calloc(M * N , sizeof( float ));
+
+		    srand(time(NULL));
+			rand_init(A, M, K);
+			rand_init(B, K, N);
+
+		    clock_gettime(CLOCK_REALTIME, &start);
+
+			ans = cake_sgemm(A, B, C, M, N, K, p, cake_cntx, NULL, 0,0,1,0,KMN);
+
+		    clock_gettime(CLOCK_REALTIME, &end);
+		    seconds = end.tv_sec - start.tv_sec;
+		    nanoseconds = end.tv_nsec - start.tv_nsec;
+		    diff_t = seconds + nanoseconds*1e-9;
+
+		    fp = fopen(fname, "a");
+		    fprintf(fp, "k-first,%d,%d,%d,%f\n",M,K,N,ans);
+		    fclose(fp);
+		
+			free(A);
+			free(B);
+			free(C);
+
+
+
+
+
+			A = (float*) malloc(M * K * sizeof( float ));
+			B = (float*) malloc(K * N * sizeof( float ));
+			C = (float*) calloc(M * N , sizeof( float ));
+
+		    srand(time(NULL));
+			rand_init(A, M, K);
+			rand_init(B, K, N);
+			
+		    clock_gettime(CLOCK_REALTIME, &start);
+
+			ans = cake_sgemm(A, B, C, M, N, K, p, cake_cntx, NULL, 0,0,1,0, MKN);
+
+		    clock_gettime(CLOCK_REALTIME, &end);
+		    seconds = end.tv_sec - start.tv_sec;
+		    nanoseconds = end.tv_nsec - start.tv_nsec;
+		    diff_t = seconds + nanoseconds*1e-9;
+
+		    fp = fopen(fname, "a");
+		    fprintf(fp, "m-first,%d,%d,%d,%f\n",M,K,N,ans);
+		    fclose(fp);
+		
+			free(A);
+			free(B);
+			free(C);
+
+
+
+
+
+			A = (float*) malloc(M * K * sizeof( float ));
+			B = (float*) malloc(K * N * sizeof( float ));
+			C = (float*) calloc(M * N , sizeof( float ));
+
+		    srand(time(NULL));
+			rand_init(A, M, K);
+			rand_init(B, K, N);
+			
+		    clock_gettime(CLOCK_REALTIME, &start);
+
+			ans = cake_sgemm(A, B, C, M, N, K, p, cake_cntx);
+
+		    clock_gettime(CLOCK_REALTIME, &end);
+		    seconds = end.tv_sec - start.tv_sec;
+		    nanoseconds = end.tv_nsec - start.tv_nsec;
+		    diff_t = seconds + nanoseconds*1e-9;
+
+		    fp = fopen(fname, "a");
+		    fprintf(fp, "opt,%d,%d,%d,%f\n",M,K,N,ans);
+		    fclose(fp);
+		
+			free(A);
+			free(B);
+			free(C);
+		}
+	}
+
+	free(cake_cntx);
+
+	return 0;
+}
+
---
+++ experiments/amd/skew/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# compile blis test
+gcc -g -O2 -std=c99 -Wall -Wno-unused-function -Wfatal-errors -fPIC  \
+-D_POSIX_C_SOURCE=200112L -fopenmp -I${CAKE_HOME}/include/blis \
+-DBLIS_VERSION_STRING=\"0.8.1-67\" -I. -c blis_test.c -o blis_test.o;
+g++ blis_test.o $CAKE_HOME/blis/lib/zen2/libblis.a  -lm -lpthread -fopenmp \
+-lrt -o blis_test;
+
+# compile cake_sgemm_test
+make;
+
+export GOMP_CPU_AFFINITY="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23";
+
+
+./cake_sgemm_test 1000 1000
+./blis_test 1000 1000
+
+# scp result_skew vikas@10.0.0.185:/Users/vikas/Documents/test
