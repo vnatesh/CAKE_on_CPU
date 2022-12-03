3ffd6dd60b912329ab8787b183a9a0d86fbbb781
vnatesh <vikas.natesh@gmail.com>
2022-12-03 13:32:58 -0500

    add cache flush between runs

--- examples/mkl_sgemm_test.c
+++ examples/mkl_sgemm_test.c
@@ -13,7 +13,16 @@
 // -Wl,--no-as-needed -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core
 //  -lmkl_gnu_thread -lpthread -lm -ldl -o mkl_sgemm_test
 
-void rand_init(float* mat, int r, int c);
+
+void rand_init(float* mat, int r, int c) {
+    // int MAX = 65536;
+    for(int i = 0; i < r*c; i++) {
+        // mat[i] = (double) i;
+        // mat[i] = 1.0;
+        // mat[i] =  (double) (i%MAX);
+        mat[i] =  (float) rand() / RAND_MAX*2.0 - 1.0;
+    }   
+}
 
 
 int main(int argc, char* argv[])  {
@@ -81,16 +90,41 @@ int main(int argc, char* argv[])  {
     rand_init(A, m, k);
     rand_init(B, k, n);
 
-    clock_gettime(CLOCK_REALTIME, &start);
+    int ntrials = atoi(argv[5]);
+    float ressss;
+    float tttmp[18];
+    int flushsz=100000000;
 
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-                m, n, k, alpha, A, k, B, n, beta, C, n);
-    clock_gettime(CLOCK_REALTIME, &end);
+    for(int i = 0; i < ntrials; i++) {
 
-    long seconds = end.tv_sec - start.tv_sec;
-    long nanoseconds = end.tv_nsec - start.tv_nsec;
-    diff_t = seconds + nanoseconds*1e-9;
-    printf("GEMM time: %f \n", diff_t); 
+
+        float *dirty = (float *)malloc(flushsz * sizeof(float));
+        #pragma omp parallel for
+        for (int dirt = 0; dirt < flushsz; dirt++){
+            dirty[dirt] += dirt%100;
+            tttmp[dirt%18] += dirty[dirt];
+        }
+
+        for(int ii =0; ii<18;ii++){
+            ressss+= tttmp[ii];
+        }
+
+
+        clock_gettime(CLOCK_REALTIME, &start);
+
+        cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                    m, n, k, alpha, A, k, B, n, beta, C, n);
+        clock_gettime(CLOCK_REALTIME, &end);
+
+        long seconds = end.tv_sec - start.tv_sec;
+        long nanoseconds = end.tv_nsec - start.tv_nsec;
+        diff_t += seconds + nanoseconds*1e-9;
+
+        free(dirty);
+    }
+
+
+    printf("GEMM time: %f \n", diff_t / ntrials); 
 
     // char fname[50];
     // snprintf(fname, sizeof(fname), "results_sq");
@@ -98,91 +132,6 @@ int main(int argc, char* argv[])  {
     // fp = fopen(fname, "a");
     // fprintf(fp, "mkl,%d,%d,%f\n",p,m,diff_t);
     // fclose(fp);
-
-
-    // printf ("\n Computations completed.\n\n");
-
-    // printf (" Top left corner of matrix A: \n");
-    // for (i=0; i<min(m,6); i++) {
-    //   for (j=0; j<min(k,6); j++) {
-    //     printf ("%12.0f", A[j+i*k]);
-    //   }
-    //   printf ("\n");
-    // }
-
-    // printf ("\n Top left corner of matrix B: \n");
-    // for (i=0; i<min(k,6); i++) {
-    //   for (j=0; j<min(n,6); j++) {
-    //     printf ("%12.0f", B[j+i*n]);
-    //   }
-    //   printf ("\n");
-    // }
-   
-    // printf ("\n Top left corner of matrix C: \n");
-    // for (i=0; i<min(m,6); i++) {
-    //   for (j=0; j<min(n,6); j++) {
-    //     printf ("%12.5G", C[j+i*n]);
-    //   }
-    //   printf ("\n");
-    // }
-
-
-
-    // double* C_check = malloc(m * n * sizeof( double ));
-    // for (int n1 = 0; n1 < n; n1++) {
-    //   for (int m1 = 0; m1 < m; m1++) {
-    //     C_check[m1*n + n1] = 0.0;
-    //     for (int k1 = 0; k1 < k; k1++) {
-    //       C_check[m1*n + n1] += A[m1*k + k1] * B[k1*n + n1];
-    //     }
-    //   }
-    // }
-
-
-    // int CORRECT = 1;
-    // int cnt = 0;
-    // int* wrong = (int*) malloc(m*n*sizeof(int));
-    // int* corrects = (int*) malloc(m*n*sizeof(int));
-
-    // for (int n1 = 0; n1 < n; n1++) {
-    //   for (int m1 = 0; m1 < m; m1++) {
-    //     // printf("%f ", C_check[m*N + n]);
-    //     if(C_check[m1*n + n1] != C[m1*n + n1]) {
-    //         wrong[cnt] = m1*n + n1;
-    //         cnt++;
-    //         CORRECT = 0;
-    //     } 
-    //   }
-    // }
-
-    // printf("\n\n");
-
-    // // printf("wrongs:");
-    // // for(int i = 0; i < 100; i++) {
-    // //     printf(" %d ", wrong[i]);
-    // // }
-    // // printf("\n\n");
-
-    // printf("wrongs:");
-    // for(int i = 0; i < 100; i++) {
-    //     printf(" %f ", C_check[wrong[i]]);
-    // }
-    // printf("\n\n");
-
-    // printf("corrects:");
-    // for(int i = 0; i < 100; i++) {
-    //     printf(" %f ", C[wrong[i]]);
-    // }
-    // printf("\n\n");
-
-
-    // if(CORRECT) {
-    //   printf("CORRECT!\n");
-    // } else {
-    //   printf("WRONG!\n");
-    //     printf("%d\n", cnt);
-    // }
-
 
 
     // printf ("\n Deallocating memory \n\n");
@@ -195,14 +144,4 @@ int main(int argc, char* argv[])  {
 }
 
 
-
-void rand_init(float* mat, int r, int c) {
-    // int MAX = 65536;
-    for(int i = 0; i < r*c; i++) {
-        // mat[i] = (double) i;
-        // mat[i] = 1.0;
-        // mat[i] =  (double) (i%MAX);
-        mat[i] =  (float) rand() / RAND_MAX*2.0 - 1.0;
-    }   
-}
 
